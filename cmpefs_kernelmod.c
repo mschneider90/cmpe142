bfa151450ad6ee74e7e9068d98d218d2bda4b5c4
mschneider90 <michael.schneider90@gmail.com>
2014-05-01 15:52:29 -0700

    Filling in superblock struct

--- cmpefs_kernelmod.c
+++ cmpefs_kernelmod.c
@@ -10,15 +10,24 @@ int cmpefs_get_sb(struct file_system_type*, int, const char*, void*, struct vfsm
 void cmpefs_kill_sb(struct super_block*);
 int cmpefs_fill_sb(struct super_block*, void*, int);
 
-struct super_block* cmpefs_sb;
-
-struct file_system_type sjfsInfo = {
+struct file_system_type cmpefs_fstype = {
 	.name = "cmpefs",
 	.owner = THIS_MODULE,
 	.get_sb = cmpefs_get_sb,
 	.kill_sb = cmpefs_kill_sb,
 	.next = NULL
 };
+
+struct super_operations cmpefs_sop;
+
+int cmpefs_fill_sb(struct super_block* sb, void* data, int silent)
+{
+	sb->s_blocksize = 1024;
+	sb->s_blocksize_bits = 8;
+	sb->s_magic = 0x534A;
+	sb->s_op = &cmpefs_sop;
+	return 0; //no error
+}
 
 int cmpefs_get_sb(struct file_system_type * fstype,
                   int flags,
@@ -31,17 +40,17 @@ int cmpefs_get_sb(struct file_system_type * fstype,
 
 void cmpefs_kill_sb(struct super_block* sb)
 {
-	kill_block_super(cmpefs_sb);
+	kill_block_super(sb);
 }
 
 static int __init cmpefs_init()
 {
-	return register_filesystem(&sjfsInfo);
+	return register_filesystem(&cmpefs_fstype);
 }
 
 static void __exit cmpefs_exit()
 {
-	unregister_filesystem(&sjfsInfo);
+	unregister_filesystem(&cmpefs_fstype);
 }
 
 module_init(cmpefs_init);
