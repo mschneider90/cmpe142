86adb5535f7ca57c09f692dc8af22cabc38c4295
mschneider90 <michael.schneider90@gmail.com>
2014-05-01 12:41:13 -0700

    Get/kill superblock functions

--- cmpefs_kernelmod.c
+++ cmpefs_kernelmod.c
@@ -9,8 +9,26 @@ extern int unregister_filesystem(struct file_system_type*);
 struct file_system_type sjfsInfo = {
 	.name = "cmpefs",
 	.owner = THIS_MODULE,
+	.get_sb = cmpefs_get_sb;
+	.kill_sb = cmpefs_kill_sb;
 	.next = NULL
 };
+
+struct super_block* cmpefs_sb;
+
+int cmpefs_get_sb(struct file_system_type * fstype,
+                  int flags,
+                  const char* dev_name,
+                  void* data,
+                  struct vfsmount* mount);
+{
+	return get_sb_nodev(fstype, flags, data, cmpefs_sb, mount);
+}
+
+void cmpefs_kill_sb(struct super_block* sb)
+{
+	kill_block_super(cmpefs_sb);
+}
 
 static int __init hello_init()
 {
